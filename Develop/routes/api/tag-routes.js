ab7cd45cc280be54ec08240f9136e442733989ba
brecec <brendaramirez@brendas-mbp.lan>
2023-07-30 12:26:41 -0500

    updates

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,21 +4,21 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
   // find all tags
   // be sure to include its associated Product data
-router.get('/', async (req, res) => {
-  try{
-    const tagData = await Tag.findAll();
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get('/', (req, res) => {
+    Tag.findAll({
+      include: [Product],
+    })
+      .then((tag)=>
+      res.json(tag)).catch((err)=>
+      res.status(500).json(err));
+  });
 
   // find a single tag by its `id`
   // be sure to include its associated Product data
 router.get('/:id', async (req, res) => {
   try{
     const tagData = await Tag.findByPk(req.params.id,{
-      include: [{model: Product, through: ProductTag, as: 'product_tag'}]
+      include: [Product]
     });
     if (!tagData) {
       res.status(404).json ({ message: 'No tag found with this id!'});
@@ -49,37 +49,9 @@ router.put('/:id', (req, res) => {
   })
     .then((tag) => {
       // find all associated tags from ProductTag
-      return Tag.findAll({ where: { tag_id: req.params.id } });
+      res.json(tag)
     })
-    .then((Tags) => {
-      // get list of current tag_ids
-      const TagIds = Tags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const tagsToRemove = Tags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: tagsToRemove } }),
-        ProductTag.bulkCreate(newTags),
-      ]);
-    })
-    .then((updatedTags) => res.json(updatedTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
-});
+  })
 
   // delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
