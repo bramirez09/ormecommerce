5bec25dd436287f636b9c8fe94b11d533c97e33f
brecec <brendaramirez@brendas-mbp.lan>
2023-07-29 21:11:15 -0500

    fixed getproduct by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, through: Tag, as: 'tagged_product' }]
+      include: [Category, { model: Tag, through: ProductTag }]
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
